1981dfc4303ff454a24ca5e1142ba1ae5b1b9aa0
koya-hikigi <hikigi@itpm-gk.com>
2019-12-23 22:10:24 +0900

    chapter3  Sample3_14～21

---
+++ chapter3/src/Sample3_14.java
@@ -0,0 +1,12 @@
+public class Sample3_14 {
+    public static void main(String[] args) {
+        int num = 5;
+        if(num < 10){
+            System.out.println("numの値は10未満です。");
+        }
+        num = 20;
+        if(num >= 100)
+            System.out.println("numの値は100以上です。");
+        System.out.println("2つ目のif文の後の処理");
+    }
+}
---
+++ chapter3/src/Sample3_20.java
@@ -0,0 +1,15 @@
+public class Sample3_20 {
+    public static void main(String[] args) {
+        int num = 3;
+        switch (num){
+            case 1:
+            case 2:
+                System.out.println("1 または 2");
+                break;
+            case 3:
+            case 4:
+                System.out.println("3 または 4");
+                break;
+        }
+    }
+}
